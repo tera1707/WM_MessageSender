8830fb6b7f337657ac2f0bcc10108ab4832d79ca
tera1707 <Masa1017>
2025-01-02 22:41:33 +0900

    ウインドウを列挙して、選んだウインドウにWM_CLOSEを送るよう修正

--- WM_MessageSender/WM_MessageSender.cpp
+++ WM_MessageSender/WM_MessageSender.cpp
@@ -9,10 +9,30 @@
 
 // グローバル変数:
 HINSTANCE hInst;                                // 現在のインターフェイス
+HWND g_hDlg;
 
 BOOL CALLBACK MyDlgProc(HWND, UINT, WPARAM, LPARAM);
 void OnButtonClicked();
+void OnSendButtonClicked();
 BOOL OnFindWindow(HWND hWnd, LPARAM lp);
+
+struct WindowData
+{
+    WindowData(HWND hwnd, BOOL isVisible, std::wstring title, std::wstring className)
+    {
+        hWnd = hwnd;
+        IsVisible = isVisible;
+        Title = title;
+        ClassName = className;
+    }
+
+    HWND hWnd;
+    BOOL IsVisible;
+    std::wstring Title;
+    std::wstring ClassName;
+};
+
+std::vector<WindowData> WindowDataList;
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -30,6 +50,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 BOOL CALLBACK MyDlgProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp)
 {
     switch (msg) {
+    case WM_INITDIALOG:
+        g_hDlg = hDlg;
+        break;
     case WM_COMMAND:
         switch (LOWORD(wp)) {
         case IDOK:
@@ -41,6 +64,9 @@ BOOL CALLBACK MyDlgProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp)
         case IDC_BUTTON1:
             OnButtonClicked();
             break;
+        case IDC_SEND_MSG:
+            OnSendButtonClicked();
+            break;
         }
         return FALSE;
     }
@@ -51,6 +77,10 @@ BOOL CALLBACK MyDlgProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp)
 
 void OnButtonClicked()
 {
+    auto listBox = GetDlgItem(g_hDlg, IDC_LIST1);
+    int pos = (int)SendMessage(listBox, LB_RESETCONTENT, 0, (LPARAM)0);
+    WindowDataList.clear();
+
     EnumWindows((WNDENUMPROC)OnFindWindow, 0);
 }
 
@@ -58,41 +88,69 @@ BOOL OnFindWindow(HWND hWnd, LPARAM lp)
 {
     WCHAR dest[MAX_PATH] = { 0 };
 
-    // プロセスID
-    DWORD processID = NULL;
-    GetWindowThreadProcessId(hWnd, &processID);
+    auto bufferSize = GetWindowTextLength(hWnd) + 1;
 
-    // プロセスハンドル
-    HANDLE hProcess = OpenProcess(
-        PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
-    if (!hProcess)
-        return false;
+    auto title = new wchar_t[bufferSize];	// newで長さlen + 1のTCHAR配列バッファを用意.
 
-    // モジュールハンドル
-    HMODULE hModule = NULL;
-    DWORD dummy = 0;
-    if (!EnumProcessModules(hProcess, &hModule, sizeof(HMODULE), &dummy))
-        return false;
-
-    // ファイル名(フルパス)
-    memset(dest, 0, MAX_PATH - 1);
-    if (!GetModuleFileNameEx(hProcess, hModule, dest, MAX_PATH - 1))
-        return false;
-
-    CloseHandle(hProcess);
-    // hModuleは自分で閉じちゃいけない
-
-    auto procPath = std::wstring(dest);
-    std::filesystem::path filepath = procPath;
-
-    if (filepath.filename() == L"Notepad.exe")
+    if (GetWindowText(hWnd, title, bufferSize) == 0)
     {
-        OutputDebugString(L"あった！");
-
-        PostMessage(hWnd, WM_CLOSE, 0, 0);
+        auto err = GetLastError();
+        return true;
     }
 
-    OutputDebugString((procPath + L"\r\n").c_str());
+    auto className = new wchar_t[256];	// newで長さlen + 1のTCHAR配列バッファを用意.
+
+    if (GetClassName(hWnd, className, 256) == 0)
+    {
+        auto err = GetLastError();
+        return true;
+    }
+
+    // 表示したい情報を揃える
+    auto isVisible = IsWindowVisible(hWnd);
+    auto titleWstr = std::wstring(title);
+    auto classNameWstr = std::wstring(className);
+    auto txt = ((isVisible == 0 ? L"×, " : L"〇, ") + titleWstr + L", " + classNameWstr + L"\r\n");
+    OutputDebugString(txt.c_str());
+
+    // ------------------------------------------
+    // 表示＆保存処理
+    // ------------------------------------------ 
+
+    // 不可視ウインドウを表示するかどうかのチェックを取得
+    auto chk = GetDlgItem(g_hDlg, IDC_CHECK1);
+    BOOL showInvisible = (int)SendMessage(chk, BM_GETCHECK, 0, (LPARAM)txt.c_str());
+
+    if (isVisible == TRUE
+        || showInvisible == TRUE)
+    {
+        // 値の保存＆表示
+        auto data = WindowData(hWnd, isVisible, titleWstr, classNameWstr);
+
+        WindowDataList.push_back(data);
+
+        auto listBox = GetDlgItem(g_hDlg, IDC_LIST1);
+        //int count = (int)SendMessage(listBox, LB_GETCOUNT, 0, (LPARAM)0);
+        int pos = (int)SendMessage(listBox, LB_ADDSTRING, 0, (LPARAM)txt.c_str());
+    }
 
     return true;
+}
+
+void OnSendButtonClicked()
+{
+    auto listBox = GetDlgItem(g_hDlg, IDC_LIST1);
+    int pos = (int)SendMessage(listBox, LB_GETCURSEL, 0, (LPARAM)0);
+
+    if (pos == LB_ERR)
+    {
+        OutputDebugString(L"ウインドウが選択されてません。");
+    }
+
+    auto selectedWindow = WindowDataList[pos];
+
+    auto ret = SendMessage(selectedWindow.hWnd, WM_CLOSE, 0, 0);
+    auto retStr = std::to_wstring(ret);
+
+    MessageBox(NULL, retStr.c_str(), L"応答", MB_OK);
 }
--- WM_MessageSender/resource.h
+++ WM_MessageSender/resource.h
@@ -14,6 +14,9 @@
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG1                     129
 #define IDC_BUTTON1                     1000
+#define IDC_SEND_MSG                    1001
+#define IDC_LIST1                       1002
+#define IDC_CHECK1                      1003
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -23,7 +26,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
